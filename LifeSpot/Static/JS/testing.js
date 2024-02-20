156a46525df71b87b183c27c28e908d11e180e6b
Name <email@exaple.ru>
2024-02-20 11:48:47 +0300

    update

---
+++ LifeSpot/Static/JS/testing.js
@@ -0,0 +1,4 @@
+﻿let userName = prompt('Введите ваше имя:');
+alert(`Здравствуй ${userName}.\nСимволов в твоем имени ${userName.length}.`);
+let elements = document.getElementsByTagName('*');
+alert("Всего строк в html: " + elements.length);
