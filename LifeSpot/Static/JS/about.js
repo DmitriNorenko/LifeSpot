8756a92aa55f86a67fa1e41ce3e373db5f4cb895
Name <email@exaple.ru>
2024-02-22 13:46:27 +0300

    feedback works

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -1,1 +1,13 @@
-﻿
+﻿let userAbout = new Map();
+function addComment(){
+    userAbout.set("name", prompt("Введите свое имя: "));
+    userAbout.set("comment", prompt("Введите комментарий: "));
+    userAbout.set("date", new Date().toLocaleString());
+    writeFeedbacks();
+}
+const writeFeedbacks = () => {
+    document.getElementsByClassName('userFeedback')[0].innerHTML += '<div class="showFeedback">\n' +
+        `<p><em>${userAbout.get('name')}  ${userAbout.get("date")}</em></p>` +
+        `<p>${userAbout.get('comment')}</p>` +
+        '</div>';
+}
